cbf532e1d9fc511b750ffdf4f9d5421abfe6f7a9
leifriksheim <riksheim.leif@gmail.com>
2019-03-27 11:03:32 -0600

    add clients

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -10,6 +10,17 @@
           For design I use <em class="em">Figma</em> or
           <em class="em">Sketch</em>.
         </p>
+        <p>
+          Some of the clients I've worked with include
+          <template v-for="(client, index) in clients">
+            <span :key="client.link">
+              <a :href="client.link" target="_blank" class="a">
+                <em class="em">{{ client.name }}</em>
+              </a>
+              <span>{{ index + 1 === clients.length ? "." : ", " }}</span>
+            </span>
+          </template>
+        </p>
       </Block>
       <Block :mt="6" :mb="7">
         <Block full :mb="4">
@@ -29,8 +40,8 @@
           <li>
             <div>
               <h3 class="h5">Front end designer</h3>
-              <a class="a"
-                >Netlife Design
+              <a class="a">
+                Netlife Design
                 <Icon name="external-link" />
               </a>
             </div>
@@ -42,8 +53,8 @@
           <li>
             <div>
               <h3 class="h5">Creative Technologist</h3>
-              <a class="a"
-                >Empefire
+              <a class="a">
+                Empefire
                 <Icon name="external-link" />
               </a>
             </div>
@@ -55,8 +66,8 @@
           <li>
             <div>
               <h3 class="h5">Photo Journalist</h3>
-              <a class="a"
-                >Aktiv i Oslo
+              <a class="a">
+                Aktiv i Oslo
                 <Icon name="external-link" />
               </a>
             </div>
@@ -75,8 +86,8 @@
           <li>
             <div>
               <h3 class="h5">Art Direction</h3>
-              <a class="a"
-                >Westerdals Scool of Communcation
+              <a class="a">
+                Westerdals Scool of Communcation
                 <Icon name="external-link" />
               </a>
             </div>
@@ -88,8 +99,8 @@
           <li>
             <div>
               <h3 class="h5">Media and Communcation</h3>
-              <a class="a"
-                >Elvebakken VGS
+              <a class="a">
+                Elvebakken VGS
                 <Icon name="external-link" />
               </a>
             </div>
@@ -112,6 +123,24 @@ import Icon from "@/components/Icon";
 export default {
   name: "home",
   components: { Block, Container, Icon },
+  data() {
+    return {
+      clients: [
+        { link: "https://www.obos.no", name: "OBOS" },
+        { link: "https://www.posten.no", name: "Posten" },
+        { link: "https://www.virke.no", name: "Virke" },
+        { link: "https://www.xxl.no", name: "XXL" },
+        { link: "https://www.gjensidige.no", name: "Gjensidige" },
+        { link: "https://www.posten.no", name: "Posten" },
+        { link: "https://www.power.no", name: "Power" },
+        { link: "https://www.dfo.no", name: "Direktoratet for Økonomistyring" },
+        {
+          link: "https://www.imdi.no",
+          name: "Integrerings- og mangfoldsdirektoratet"
+        }
+      ]
+    };
+  },
   created() {
     document.body.classList.remove("inverted");
   }
